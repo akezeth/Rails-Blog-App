360a981d75afffd73b25ac0420b62178b4576441
akezeth <akezeth@gmail.com>
2023-09-20 17:38:11 +0300

    Add associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', counter_cache: :posts_counter
+  has_many :likes
+  has_many :comments
 end
