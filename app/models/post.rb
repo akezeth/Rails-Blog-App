8acb0b1144b300018d9fa2d758f7ec47b0513c1d
akezeth <akezeth@gmail.com>
2023-09-21 11:03:33 +0300

    Add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: :posts_counter
   has_many :likes
   has_many :comments
+
+  def recent_five_comments
+    comments.limit(5).order(created_at: :DESC)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-     has_many :posts
-     has_many :likes
-     has_many :comments
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes
+  has_many :comments
+
+  def recent_three_posts
+    posts.limit(3).order(created_at: :DESC)
+  end
 end
